c724452d1af1bb1b492ffd22037255474002bdb9
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2020-10-16 23:52:42 +0800

    code refactor

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 import Visualization from "./components/visualization/Visualization";
 
-
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
   // Set auth token header auth
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,20 +10,38 @@ class Navbar_ extends Component {
           <div className="left" style={{ padding: "5px" }}>
             <img alt="" src={logo} width="60" height="60" />
           </div>
-          <div className="left black-text" style={{padding: "0px 10px", fontSize: "25px"}}>House Matchers</div>
-          <div className="left" style={{padding: "0px 10px"}}>
-            <Link className="black-text" to="/dashboard">Home</Link>
+          <div
+            className="left black-text"
+            style={{ padding: "0px 10px", fontSize: "25px" }}
+          >
+            House Matchers
           </div>
-          <div className="left" style={{padding: "0px 10px"}}>
-            <Link className="black-text" to="/recommed">Recommend</Link>
+          <div className="left" style={{ padding: "0px 10px" }}>
+            <Link className="black-text" to="/dashboard">
+              Home
+            </Link>
           </div>
-          <div className="left" style={{padding: "0px 10px"}}>
-            <Link className="black-text" to="/visualization">Visualization</Link>
+          <div className="left" style={{ padding: "0px 10px" }}>
+            <Link className="black-text" to="/recommed">
+              Recommend
+            </Link>
           </div>
-          <div className="left" style={{padding: "0px 10px"}}>
-            <Link className="black-text" to="/contact">Contact</Link>
+          <div className="left" style={{ padding: "0px 10px" }}>
+            <Link className="black-text" to="/visualization">
+              Visualization
+            </Link>
           </div>
-          <a href="#" className="right black-text" data-target="slide-out" style={{ padding: "0px 20px" }}>
+          <div className="left" style={{ padding: "0px 10px" }}>
+            <Link className="black-text" to="/contact">
+              Contact
+            </Link>
+          </div>
+          <a
+            href="#"
+            className="right black-text"
+            data-target="slide-out"
+            style={{ padding: "0px 20px" }}
+          >
             <i className="material-icons">settings</i>
           </a>
           <div className="right black-text" style={{ padding: "0px 10px" }}>
