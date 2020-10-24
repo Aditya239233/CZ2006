0b2fc1257ce747a61d983253457ef364e05a8b2c
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2020-10-25 01:05:27 +0800

    visualization layout

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,6 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            {console.log(store.getState())}
             <Navbar_ />
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
--- client/src/components/visualization/Visualization.js
+++ client/src/components/visualization/Visualization.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
+import "./Visualization.css";
 
 class Visualization extends Component {
+  constructor() {
+    super();
+    this.state = { data: "Heatmap" };
+  }
+
+  btnClick(val) {
+    this.setState({ data: val });
+  }
+
   render() {
     return (
-      <div>
+      <div className="wrapper">
+        <div className="inner">
+          <button
+            className="heading"
+            onClick={this.btnClick.bind(this, "Heatmap")}
+          >
+            Heatmap
+          </button>
+        </div>
+        <div className="inner">
+          <button
+            className="heading"
+            onClick={this.btnClick.bind(this, "Bar Graph")}
+          >
+            Bar Graph
+          </button>
+        </div>
+        <div className="inner">
+          <button
+            className="heading"
+            onClick={this.btnClick.bind(this, "Time Series")}
+          >
+            Time Series
+          </button>
+        </div>
       </div>
     );
   }
