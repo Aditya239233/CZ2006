19a8047f05454598de91ba07a633ac3241b3a7ab
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2020-11-03 23:25:15 +0800

    reading csv

--- routes/api/data.js
+++ routes/api/data.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const csv = require('csvtojson');
 
 router.get("/data", (req, res) => {
-  // placeholder for csv file
-  const data = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "Brad", lastName: "Traversy" },
-    { id: 3, firstName: "Mary", lastName: "Swanson" },
-  ];
-
-  res.json(data);
+  const converter = csv().fromFile('./data/data.csv').then((json) => {
+    res.json(json);
+  })
 });
 
 module.exports = router;
