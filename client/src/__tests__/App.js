ac277668a99adf01826ae608518b1de88f14be74
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2020-11-06 18:02:53 +0800

    App level tests

---
+++ client/src/__tests__/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+test("renders the component correctly - header", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText("Find your ideal home");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders the component correctly - content", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(
+    "Use House Matcher's property recommendation tool to search for a suitable resale HDB"
+  );
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders the component correctly - logged in', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText("Not Logged In");
+  expect(linkElement).toBeInTheDocument();
+});
+
--- routes/api/data.js
+++ routes/api/data.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const csv = require('csvtojson');
+const csv = require("csvtojson");
 
 router.get("/data", (req, res) => {
-  const converter = csv().fromFile('./data/data.csv').then((json) => {
-    res.json(json);
-  })
+  const converter = csv()
+    .fromFile("./data/data.csv")
+    .then((json) => {
+      res.json(json);
+    });
 });
 
 module.exports = router;
